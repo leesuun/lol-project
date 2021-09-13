8ce91d36c80c55a22a7773972dafc43fd574e453
leesuun <77526673+leesuun@users.noreply.github.com>
2021-09-13 21:22:43 +0900

    login-layout

---
+++ src/controllers/globalcontroller/join.js
@@ -0,0 +1,5 @@
+export const getJoin = (req, res) => {
+    return res.render("join");
+};
+
+export const postJoin = (req, res) => {};
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import { home } from "../controllers/globalcontroller/home";
+import { getJoin, postJoin } from "../controllers/globalcontroller/join";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
+globalRouter.route("/join").get(getJoin).post(postJoin);
 
 export default globalRouter;
