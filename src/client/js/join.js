743345c02581987edfc9b52600041b74cf55b288
leesuun <77526673+leesuun@users.noreply.github.com>
2021-09-23 21:03:59 +0900

    id,pass,pass2 fetch

--- src/client/js/join.js
+++ src/client/js/join.js
@@ -1,10 +1,99 @@
 const form = document.getElementById("joinForm");
 const userId = form.querySelector("#userId");
+const stateId = form.querySelector("#stateId");
+const password = form.querySelector("#password");
+const statePass = form.querySelector("#statePass");
+const password2 = form.querySelector("#password2");
+const statePass2 = form.querySelector("#statePass2");
 
-const handleIdBlur = (event) => {
-    fetch("/api/join", {
+const ok = {
+    id: false,
+    password: false,
+    password2: false,
+};
+
+userId.focus();
+
+const handleIdBlur = async (event) => {
+    const inputId = userId.value;
+
+    if (inputId === "") {
+        console.log(inputId);
+    }
+
+    const response = await fetch("/api/join", {
         method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            userId: inputId !== "" ? inputId : "blank",
+        }),
     });
+
+    if (response.status === 200) {
+        const state = await response.json();
+        console.log(state);
+        ok.id = state.ok;
+        stateId.innerText = state.msg;
+    }
+};
+
+const handlePassBlur = async (event) => {
+    const inputPass = password.value;
+
+    const response = await fetch("/api/join", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            password: inputPass !== "" ? inputPass : "blank",
+        }),
+    });
+
+    if (response.status === 200) {
+        const state = await response.json();
+        statePass.innerText = state.msg;
+        console.log(state);
+        ok.password = state.ok;
+    }
+};
+
+const handlePass2Blur = async (evnet) => {
+    const inputPass = password.value;
+    const inputPass2 = password2.value;
+
+    const response = await fetch("/api/join", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            password2: inputPass2 !== "" ? inputPass2 : "blank",
+            password: inputPass,
+        }),
+    });
+
+    if (response.status === 200) {
+        const state = await response.json();
+        statePass2.innerText = state.msg;
+        console.log(state);
+        ok.password2 = state.ok;
+    }
+};
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (ok.id === false || ok.password === false || ok.password2 === false) {
+        console.log("false");
+    } else {
+        console.log("true");
+    }
 };
 
 userId.addEventListener("blur", handleIdBlur);
+password.addEventListener("blur", handlePassBlur);
+password2.addEventListener("blur", handlePass2Blur);
+form.addEventListener("submit", handleSubmit);
--- src/controllers/globalcontroller/join.js
+++ src/controllers/globalcontroller/join.js
@@ -9,10 +9,60 @@ export const postJoin = async (req, res) => {
     const {
         body: { userId, password, password2, nickname },
     } = req;
+
+    res.end();
 };
 
-export const inputState = (req, res) => {
-    console.log("hi");
+export const inputState = async (req, res) => {
+    const {
+        body: { userId, password, password2 },
+    } = req;
+
+    const state = {
+        msg: null,
+        ok: false,
+    };
+
+    const user = await User.findOne({ userId });
+    if (userId) {
+        state.ok = false;
+        if (user) {
+            state.msg = "이미 사용중이거나 탈퇴한 아이디입니다.";
+        } else if (userId === "blank") {
+            state.msg = "필수 입력 항목입니다.";
+        } else if (!regex.userid.test(userId)) {
+            state.msg = "6~15자 영문,숫자가 포함되어야 합니다.";
+        } else {
+            state.msg = "사용가능한 아이디 입니다.";
+            state.ok = true;
+        }
+    }
+
+    if (password) {
+        state.ok = false;
+        if (password === "blank") {
+            state.msg = "필수 입력 항목입니다.";
+        } else if (!regex.password.test(password)) {
+            state.msg = "6~20자 영문, 숫자 또는 특수문자가 포함되어야 합니다.";
+        } else {
+            state.msg = "사용가능한 비밀번호 입니다.";
+            state.ok = true;
+        }
+    }
+
+    if (password2) {
+        state.ok = false;
+        if (password2 === "blank") {
+            state.msg = "필수 입력 항목입니다.";
+        } else if (password !== password2) {
+            state.msg = "일치하지 않는 비밀번호입니다.";
+        } else {
+            state.msg = "비밀번호가 일치 합니다.";
+            state.ok = true;
+        }
+    }
+
+    res.send(state);
 };
 
 // 공백 해결
