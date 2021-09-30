1bc9e933947d6549c85bd3fb16f17672832fc581
leesuun <77526673+leesuun@users.noreply.github.com>
2021-09-30 18:37:55 +0900

    lotation

--- src/controllers/globalcontroller/home.js
+++ src/controllers/globalcontroller/home.js
@@ -1,5 +1,35 @@
 import fetch from "node-fetch";
 
-export const home = (req, res) => {
-    return res.render("home");
+async function getLotation() {
+    const LOTATION_URL = `platform/v3/champion-rotations?api_key=${process.env.API_KEY}`;
+    const ALLCHAMPION_URL = `${process.env.LOL_CDN_URL}data/en_US/champion.json`;
+    const lotationData = await (
+        await fetch(`${process.env.LOL_BASE_URL}${LOTATION_URL}`)
+    ).json();
+    const lotationNumber = lotationData.freeChampionIds.map((key) =>
+        String(key)
+    );
+
+    const allChampionInfo = await (await fetch(ALLCHAMPION_URL, {})).json();
+    const allChampionAry = Object.entries(allChampionInfo.data);
+
+    const lotationImg = [];
+
+    for (let i = 0; i < allChampionAry.length; i++) {
+        if (lotationNumber.includes(allChampionAry[i][1].key)) {
+            lotationImg.push(
+                process.env.LOL_CDN_URL +
+                    "img/champion/" +
+                    allChampionAry[i][1].image.full
+            );
+        }
+    }
+    lotationImg.sort();
+    return lotationImg;
+}
+
+export const home = async (req, res) => {
+    const lotationImgs = await getLotation();
+
+    return res.render("home", { lotationImgs });
 };
--- src/controllers/globalcontroller/lotation.js
+++
@@ -1,7 +0,0 @@
-import fetch from "node-fetch";
-
-export const getLotation = (req, res) => {
-    console.log("hi");
-    fetch("/");
-    return res.end();
-};
