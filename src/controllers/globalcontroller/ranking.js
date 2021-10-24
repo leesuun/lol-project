e20bf206de067369e0d7bd3439d6f0656038a2d4
leesuun <77526673+leesuun@users.noreply.github.com>
2021-10-24 17:44:46 +0900

    Update ranking.js

--- src/controllers/globalcontroller/ranking.js
+++ src/controllers/globalcontroller/ranking.js
@@ -1,13 +1,16 @@
 import fetch from "node-fetch";
 export const ranking = async (req, res) => {
-    let page = Number(req.params.page) < 1 ? 1 : Number(req.params.page);
+    let page = Number(req.params.page);
     const RANKING_URL = `league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=${process.env.API_KEY}`;
-
     const response = await fetch(`${process.env.LOL_BASE_URL}${RANKING_URL}`);
     let data = null;
     let challenger = null;
-    const limit = 50;
+    const limit = 15;
     const skip = page * limit;
+
+    if (1 > page || page > 6) {
+        return res.redirect("/");
+    }
 
     if (response.status === 200) {
         data = await response.json();
@@ -16,9 +19,20 @@ export const ranking = async (req, res) => {
         });
     }
 
-    if (page === 1) {
-        challenger = challenger.slice(0, skip);
-    } else if (page > 1) {
+    for (let i = skip - 15; i < skip; i++) {
+        const ACCOUNT_URL = `summoner/v4/summoners/${challenger[i].summonerId}?api_key=${process.env.API_KEY}`;
+        const userInfo = Promise.all([
+            fetch(`${process.env.LOL_BASE_URL}${ACCOUNT_URL}`)
+                .then((response) => response.json())
+                .then((json) => {
+                    console.log(json);
+                }),
+        ]);
+    }
+
+    // return res.send(ary);
+
+    if (page >= 1) {
         challenger = challenger.slice(skip - 50, skip);
     }
 
